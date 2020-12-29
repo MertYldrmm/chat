e78aaf472bb5d9547b27e0f30aadaf6724dfd04b
MertYldrmm <mert.yyldrim@gmail.com>
2020-12-29 20:50:02 +0300

    socket.io redis adapter

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,3 +1,7 @@
 app.controller('chatController', ['$scope', ($scope) => {
     const socket = io();
+
+    socket.on('hello', () => {
+        console.log('hello');
+    });
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -4,9 +4,19 @@ const io =socketio();
 const socketApi = {
     io
 };
+/**
+ * Redis adapter
+ */
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({ 
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
     console.log('a user logged in');
+
+    socket.broadcast.emit('hello');
 });
 
 module.exports = socketApi;
