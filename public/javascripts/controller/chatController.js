1e9e54ce6806b5ee83d7193b1bbf80fcf115a69b
MertYldrmm <mert.yyldrim@gmail.com>
2020-12-30 01:30:39 +0300

    chat ui: tab

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,7 +1,10 @@
 app.controller('chatController', ['$scope', ($scope) => {
+    $scope.activeTab = 2;
+
+    $scope.changeTab = tab => {
+        $scope.activeTab = tab;
+    };
+
     const socket = io();
 
-    socket.on('hello', () => {
-        console.log('hello');
-    });
 }]);
